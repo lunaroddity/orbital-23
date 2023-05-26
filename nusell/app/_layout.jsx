e96897ea6ea9e56f16fb95a60838de4225cb8353
YingXuan Lim <limyx27@gmail.com>
2023-05-27 00:02:14 +0800

    Fix bug

--- nusell/app/_layout.jsx
+++ nusell/app/_layout.jsx
@@ -3,7 +3,7 @@ import { AuthProvider } from "../contexts/auth";
 
 export default function RootLayout() {
     return (
-        <AuthProvider> {/* Only those logged in are permitted to view the contents of the app. */}
+        <AuthProvider>
             <Slot />
         </AuthProvider>
     )
