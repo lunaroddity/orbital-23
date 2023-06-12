2eb40b40d10f4bbee635f18d8e16bf2234516207
aliciad <e0968302@u.nus.edu>
2023-06-12 09:24:23 +0800

    Update registration page

--- nusell/app/(auth)/register.jsx
+++ nusell/app/(auth)/register.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import { supabase } from '../../lib/supabase';
-import { View, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 import { Text, TextInput, ActivityIndicator, Button } from 'react-native-paper';
 import { HeaderBar } from './_layout';
+import * as ImagePicker from "expo-image-picker";
 
 export default function RegisterPage() {
     const [firstName, setFirstName] = useState('');
@@ -13,7 +14,20 @@ export default function RegisterPage() {
     const [confirmPassword, setConfirmPassword] = useState('');
     const [loading , setLoading] = useState(false);
     const [errMsg, setErrMsg] = useState('');
+    const [avatar, setAvatar] = useState(null);
     
+    const handleAddAvatar = async () => {
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images
+        })
+
+        if (!result.canceled) {
+            // setImage accesses only the first image and gets its URI.
+            setAvatar(result.assets[0].uri);
+        }
+        console.log(`result: ${JSON.stringify(result)}`);
+    }
+
     const handleSubmit = async () => {
         if (firstName === '') {
             setErrMsg('First Name cannot be empty.');
@@ -57,12 +71,42 @@ export default function RegisterPage() {
             return;
         }
 
+        let imageUrl = null;
+        if (avatar != null) {
+            // To be replaced with UUID in future to prevent clashes in naming.
+            const { data, error } = await supabase.storage.from('user information').upload(`${new Date().getTime()}`, 
+                {
+                    uri: avatar,
+                    type: 'jpg',
+                    name: 'name'
+                });
+
+            // If image cannot be uploaded, stop loading icon and show error message
+            if (error) {
+                setLoading(false);
+                setErrMsg(error.message);
+                return;
+            }
+            // Pulls image URL from supabase.
+            const { data: { publicUrl } } = supabase.storage.from('user information').getPublicUrl(data.path);
+            imageUrl = publicUrl;   
+        }
+
 
         setLoading(true); // Renders a spinning loading icon.
-        const { error } = supabase.auth.signUp({ email, password });
+        const { userError } = await supabase.from('user information')
+            .insert({ firstName: firstName, lastName: lastName, username: username, avatar: imageUrl})
+            .select()
+            .single();
+
+        const { authError } = supabase.auth.signUp({ email, password });
+
         setLoading(false); // Stops rendering loading icon.
-        if (error) {
-            setErrMsg(error.message);
+        if (userError) {
+            setErrMsg(userError.message);
+            return;
+        } else if (authError) {
+            setErrMsg(authError.message);
             return;
         }
     }
@@ -71,6 +115,10 @@ export default function RegisterPage() {
         <View style = {styles.view}>
             <HeaderBar />
 
+            <Image style= {styles.avatar} source={{ uri: avatar }} />
+
+            <Button onPress={handleAddAvatar}>Add Image</Button>
+
             {/* FirstName input */}
             <TextInput
                 style={styles.emailInput}
@@ -78,7 +126,7 @@ export default function RegisterPage() {
                 label='First Name'
                 activeOutlineColor='#003D7C'
                 autoCapitalize='none'
-                textContentType='emailAddress'
+                textContentType='name'
                 value={firstName}
                 onChangeText={setFirstName} />
 
@@ -89,7 +137,7 @@ export default function RegisterPage() {
                 label='Last Name'
                 activeOutlineColor='#003D7C'
                 autoCapitalize='none'
-                textContentType='emailAddress'
+                textContentType='name'
                 value={lastName}
                 onChangeText={setLastName} />
 
@@ -100,7 +148,7 @@ export default function RegisterPage() {
                 label='Username'
                 activeOutlineColor='#003D7C'
                 autoCapitalize='none'
-                textContentType='username'
+                textContentType='nickname'
                 value={username}
                 onChangeText={setUsername} />
 
@@ -134,7 +182,7 @@ export default function RegisterPage() {
                 label='Confirm Password'
                 activeOutlineColor='#003D7C'
                 autoCapitalize='none'
-                textContentType='Confirm Password'
+                textContentType='none'
                 value={confirmPassword}
                 onChangeText={setConfirmPassword} />
 
@@ -171,5 +219,13 @@ const styles = StyleSheet.create({
         marginBottom: 15,
         backgroundColor: '#BF3E3E',
         color: '#FFFFFF'
-    }
+    },
+    avatar: {
+        width: 70,
+        height: 70,
+        borderRadius: 50,
+        marginRight: 5,
+        borderColor: '#003D7C',
+        borderWidth: 2
+      },
 });
