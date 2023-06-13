d787ca9dd443fdb306ceed0355c7f0eeab720d2a
aliciad <e0968302@u.nus.edu>
2023-06-13 15:49:25 +0800

    Edit registration page

--- nusell/app/(auth)/register.jsx
+++ nusell/app/(auth)/register.jsx
@@ -1,46 +1,20 @@
 import { useState } from 'react';
 import { supabase } from '../../lib/supabase';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Text, TextInput, ActivityIndicator, Button } from 'react-native-paper';
 import { HeaderBar } from './_layout';
-import * as ImagePicker from "expo-image-picker";
+import { showMessage } from "react-native-flash-message";
+
 
 export default function RegisterPage() {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const[password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [loading , setLoading] = useState(false);
     const [errMsg, setErrMsg] = useState('');
-    const [avatar, setAvatar] = useState(null);
-    
-    const handleAddAvatar = async () => {
-        const result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.Images
-        })
-
-        if (!result.canceled) {
-            // setImage accesses only the first image and gets its URI.
-            setAvatar(result.assets[0].uri);
-        }
-        console.log(`result: ${JSON.stringify(result)}`);
-    }
 
     const handleSubmit = async () => {
-        if (firstName === '') {
-            setErrMsg('First Name cannot be empty.');
-            return;
-        }
-        if (lastName === '') {
-            setErrMsg('Last Name cannot be empty.');
-            return;
-        }
-        if (username === '') {
-            setErrMsg('Username cannot be empty.');
-            return;
-        }
+       
         if (email === '') {
             setErrMsg('Email cannot be empty.');
             return;
@@ -71,86 +45,30 @@ export default function RegisterPage() {
             return;
         }
 
-        let imageUrl = null;
-        if (avatar != null) {
-            // To be replaced with UUID in future to prevent clashes in naming.
-            const { data, error } = await supabase.storage.from('user information').upload(`${new Date().getTime()}`, 
-                {
-                    uri: avatar,
-                    type: 'jpg',
-                    name: 'name'
-                });
-
-            // If image cannot be uploaded, stop loading icon and show error message
-            if (error) {
-                setLoading(false);
-                setErrMsg(error.message);
-                return;
-            }
-            // Pulls image URL from supabase.
-            const { data: { publicUrl } } = supabase.storage.from('user information').getPublicUrl(data.path);
-            imageUrl = publicUrl;   
-        }
-
-
         setLoading(true); // Renders a spinning loading icon.
-        const { userError } = await supabase.from('user information')
-            .insert({ firstName: firstName, lastName: lastName, username: username, avatar: imageUrl})
-            .select()
-            .single();
 
-        const { authError } = supabase.auth.signUp({ email, password });
+        const { error: authError } = supabase.auth.signUp(
+            { 
+                email: email, 
+                password: password,
+                
+            })
 
-        setLoading(false); // Stops rendering loading icon.
-        if (userError) {
-            setErrMsg(userError.message);
-            return;
-        } else if (authError) {
+        if (authError) {
             setErrMsg(authError.message);
             return;
+        } else {
+            showMessage({ message: "success!", type: "success",
+        });
         }
+
+        setLoading(false); // Stops rendering loading icon.        
+
     }
 
     return (
         <View style = {styles.view}>
             <HeaderBar />
-
-            <Image style= {styles.avatar} source={{ uri: avatar }} />
-
-            <Button onPress={handleAddAvatar}>Add Image</Button>
-
-            {/* FirstName input */}
-            <TextInput
-                style={styles.emailInput}
-                mode='outlined'
-                label='First Name'
-                activeOutlineColor='#003D7C'
-                autoCapitalize='none'
-                textContentType='name'
-                value={firstName}
-                onChangeText={setFirstName} />
-
-            {/* LastName input */}
-            <TextInput
-                style={styles.emailInput}
-                mode='outlined'
-                label='Last Name'
-                activeOutlineColor='#003D7C'
-                autoCapitalize='none'
-                textContentType='name'
-                value={lastName}
-                onChangeText={setLastName} />
-
-            {/* Username input */}
-            <TextInput
-                style={styles.emailInput}
-                mode='outlined'
-                label='Username'
-                activeOutlineColor='#003D7C'
-                autoCapitalize='none'
-                textContentType='nickname'
-                value={username}
-                onChangeText={setUsername} />
 
             {/* Email input */}
             <TextInput
@@ -182,7 +100,7 @@ export default function RegisterPage() {
                 label='Confirm Password'
                 activeOutlineColor='#003D7C'
                 autoCapitalize='none'
-                textContentType='none'
+                textContentType='password'
                 value={confirmPassword}
                 onChangeText={setConfirmPassword} />
 
