f5eedf5ea7ee82a5caff302a20d41b036d0446c9
Lim YingXuan <limyx27@gmail.com>
2023-07-20 15:38:56 +0800

    Update package.json and package-lock.json

--- nusell/app/(home)/(chat)/useChatClient.js
+++ nusell/app/(home)/(chat)/useChatClient.js
@@ -21,8 +21,8 @@ export const useChatClient = () => {
       const { data } = await supabase.auth.getSession();
       const key = process.env.JWT
       const sessionData = JWT.decode(data.session.access_token, key);
-      const chatUserToken = JWT.encode({ user_id: user.id, ...sessionData }, key);
       console.log(`sessionData: ${JSON.stringify(sessionData)}`);
+      const chatUserToken = JWT.encode({ user_id: user.id, ...sessionData }, key);
       console.log(`token: ${JSON.stringify(chatUserToken)}`);
       
       try {
